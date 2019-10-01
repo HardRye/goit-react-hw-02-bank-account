41291d9eb96beaaa75fae77fdb7368103be5b96c
HardRye <nikolaimihail@gmail.com>
2019-10-01 19:32:36 +0300

    Updates

---
+++ precache-manifest.06b61cb19195292f60c541b0bcaa8d51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05769a3b07bf0c62ab2e5e16e3a33206",
+    "url": "/goit-react-hw-02-bank-account/index.html"
+  },
+  {
+    "revision": "9c48805740dd18a1794e",
+    "url": "/goit-react-hw-02-bank-account/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "5a55e4b5ebae7826b3c1",
+    "url": "/goit-react-hw-02-bank-account/static/css/main.227c459c.chunk.css"
+  },
+  {
+    "revision": "9c48805740dd18a1794e",
+    "url": "/goit-react-hw-02-bank-account/static/js/2.31afc9d8.chunk.js"
+  },
+  {
+    "revision": "5a55e4b5ebae7826b3c1",
+    "url": "/goit-react-hw-02-bank-account/static/js/main.5fe31e69.chunk.js"
+  },
+  {
+    "revision": "e55f51874e9b7fc8b03d",
+    "url": "/goit-react-hw-02-bank-account/static/js/runtime~main.c691db01.js"
+  }
+]);
