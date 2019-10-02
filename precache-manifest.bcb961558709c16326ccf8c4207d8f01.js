3bec4c6c8170b3f2fb9f8426da263d20279424b7
HardRye <nikolaimihail@gmail.com>
2019-10-02 21:52:27 +0300

    Updates

--- precache-manifest.bcb961558709c16326ccf8c4207d8f01.js
+++ precache-manifest.bcb961558709c16326ccf8c4207d8f01.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "05769a3b07bf0c62ab2e5e16e3a33206",
+    "revision": "c06134484d43aa42342378d71e375a97",
     "url": "/goit-react-hw-02-bank-account/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/goit-react-hw-02-bank-account/static/css/2.39017165.chunk.css"
   },
   {
-    "revision": "5a55e4b5ebae7826b3c1",
+    "revision": "85bc6d811d812df21e8e",
     "url": "/goit-react-hw-02-bank-account/static/css/main.227c459c.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/goit-react-hw-02-bank-account/static/js/2.31afc9d8.chunk.js"
   },
   {
-    "revision": "5a55e4b5ebae7826b3c1",
-    "url": "/goit-react-hw-02-bank-account/static/js/main.5fe31e69.chunk.js"
+    "revision": "85bc6d811d812df21e8e",
+    "url": "/goit-react-hw-02-bank-account/static/js/main.c0315130.chunk.js"
   },
   {
     "revision": "e55f51874e9b7fc8b03d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-02-bank-account/precache-manifest.06b61cb19195292f60c541b0bcaa8d51.js"
+  "/goit-react-hw-02-bank-account/precache-manifest.bcb961558709c16326ccf8c4207d8f01.js"
 );
 
 self.addEventListener('message', (event) => {
