3c976e3f6cb71ee2d899458cfd8cf58d8ec83067
HardRye <nikolaimihail@gmail.com>
2019-10-02 22:51:53 +0300

    fix

--- src/components/BankAccount/Balance/Balance.jsx
+++ src/components/BankAccount/Balance/Balance.jsx
@@ -3,24 +3,22 @@ import PropTypes from 'prop-types';
 import { getSum } from '../helpers';
 import styles from './Balance.module.css';
 
-const Balance = ({ transactions }) => {
-  return (
-    <section className={styles.balance}>
-      <span role="img" aria-label="deposit" className={styles.arrowUp}>
-        ⬆️
-        {getSum(transactions, 'Deposit')}$
-      </span>
-      <span role="img" aria-label="widthdrawal" className={styles.arrowDown}>
-        ⬇️
-        {getSum(transactions, 'Withdrawal')}$
-      </span>
-      <span>
-        Balance:
-        {getSum(transactions, 'Deposit') - getSum(transactions, 'Withdrawal')}$
-      </span>
-    </section>
-  );
-};
+const Balance = ({ transactions }) => (
+  <section className={styles.balance}>
+    <span role="img" aria-label="deposit" className={styles.arrowUp}>
+      ⬆️
+      {getSum(transactions, 'Deposit')}$
+    </span>
+    <span role="img" aria-label="widthdrawal" className={styles.arrowDown}>
+      ⬇️
+      {getSum(transactions, 'Withdrawal')}$
+    </span>
+    <span>
+      Balance:
+      {getSum(transactions, 'Deposit') - getSum(transactions, 'Withdrawal')}$
+    </span>
+  </section>
+);
 
 Balance.propTypes = {
   transactions: PropTypes.arrayOf(
